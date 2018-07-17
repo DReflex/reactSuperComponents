116b7d496d90e6e8d6473b8e432676166f2c3cad
DReflex <tkd.vatra@hotmail.com>
2018-07-17 02:16:08 +0200

    adding minimal options to branding

--- src/app/components/navigation/branding.js
+++ src/app/components/navigation/branding.js
@@ -2,8 +2,9 @@ import React from "react";
 
 function Branding(props){
   return(
-    <div className="brand">
-      <h2>{props.brand}</h2>
+    <div style={{fontFamily: props.brand.fontFamily}} id="navigationBrand" className="brand">
+      {(props.brand.logo) ? <img src={props.brand.logo} alt="#_#" />:"" }
+      <h2>{props.brand.title}</h2>
     </div>
   )
 }
--- src/app/components/standoutPage.js
+++ src/app/components/standoutPage.js
@@ -51,7 +51,11 @@ const navigationElements={
     }
   },
   //if none specified no show
-  brand:"standout",
+  brand:{
+    title:"standout",
+    fontFamily:"'Roboto Condensed', sans-serif",
+    logo:"http://emblemsbf.com/img/15633.jpg"
+  },
   //if none specified default is transparent as described by css
   background:"transparent",
   isScrolling:"#07003e",
