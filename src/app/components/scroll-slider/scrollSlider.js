deef6250f6a9d1eae522cf815477c7b7546d88ee
DReflex <tkd.vatra@hotmail.com>
2018-07-29 05:18:08 +0200

    basic functionality of scroll slider

--- src/app/components/scroll-slider/scrollSlider.js
+++ src/app/components/scroll-slider/scrollSlider.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import smoothscroll from 'smoothscroll-polyfill';
 import './scrollSlider.scss'
 class ScrollSlider extends React.Component{
   constructor(props){
@@ -33,10 +34,12 @@ class ScrollSlider extends React.Component{
     let check = checkElement("scrollSlider");
     if(check){
       const element = getChild()
+      const controller = document.getElementById("controller")
+
       this.setState(
         (state) =>{
         let new_items = state.items.map((item) => {
-          if(item.title === element){
+          if(item.title === element.id){
             return {
               ...item,
               transform:"translate3d(0,0,0)"
@@ -54,24 +57,13 @@ class ScrollSlider extends React.Component{
           items:new_items
         };
       })
+      controller.style.transform = `translate3d(${controller.offsetWidth * element.num}px,0,0)`
+
     }
 
 
 
   }
-  //
-  // return state.map((pin) =>{
-  //   if(pin.id === action.id){
-  //     return {
-  //       ...pin,
-  //       who:action.who,
-  //       votes:action.votes
-  //     }
-  //   }else{
-  //     return pin
-  //   }
-  // })
-
 
   render(){
     let sliderItems = this.state.items;
@@ -82,7 +74,7 @@ class ScrollSlider extends React.Component{
           </div>
 
           <div className="slider-navigation">
-            <SliderNavigation/>
+            <SliderNavigation items={sliderItems}/>
           </div>
       </div>
     )
@@ -98,7 +90,11 @@ function getChild(){
   const child_nodes = id.childNodes
   for (let i = 1; i < child_nodes.length +1; i++) {
     if(current_scroll_position <= window.innerHeight * i){
-      return child_nodes[i -1].id
+      return {
+        id:child_nodes[i -1].id,
+        num: i -1
+      }
+
     }
     else{
       child_nodes[i-1].style.transform = "translate3d(100%, 0,0)"
@@ -135,10 +131,21 @@ function SubComponent (props){
   )
 }
 function SliderNavigation (props){
-  const SliderComponent = () =>{
+  const LEN = props.items
+  const handleClick = (num) =>{
+    const element = document.getElementById("about");
+    let scrollValue= element.offsetTop + 50 + (num * window.innerHeight);
+    window.scroll({ top: scrollValue, left: 0, behavior: 'smooth' });
+
+
+  }
+  const SliderComponent = (num) =>{
+    const SCRR = (num) =>{
+
+    }
     return(
       <div className="slider-component">
-        <div onClick={() => console.log("clicked")} className="circle"></div>
+        <div onClick={() => (!isNaN(num.num))? handleClick(num.num):console.log(0)} className="circle"></div>
         <div className="line"></div>
       </div>
     )
@@ -146,11 +153,8 @@ function SliderNavigation (props){
   return(
     <div>
       <div className="slider-outliner">
-        <SliderComponent/>
-        <SliderComponent />
-        <SliderComponent />
-        <SliderComponent />
-        <div className="slider-controller">
+        {LEN.map((item, num) => <SliderComponent key={num} num={num} />)}
+        <div id="controller" className="slider-controller">
           <SliderComponent />
         </div>
       </div>
